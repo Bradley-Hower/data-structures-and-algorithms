33133566cff1258525c98fdf5f15ce3f5d72c8e5
Bradley Hower <bradleyhower@proton.me>
2024-01-12 20:56:21 -0800

    linked list completed

--- python/assignments/README.md
+++ python/assignments/README.md
@@ -1,7 +1,14 @@
-# Code 401 Python - Code Challenges
+# Code 401 Python
 
 ## *Table of Contents*
 
-- Entry 1: [Reverse an Array](https://bradley-hower.github.io/data-structures-and-algorithms-401/code-challenges/class-01)
-- Entry 2: [Array Insert Shift](https://bradley-hower.github.io/data-structures-and-algorithms-401/code-challenges/class-02)
-- Entry 3: [Binary Search](https://bradley-hower.github.io/data-structures-and-algorithms-401/code-challenges/class-03)
+Module 1
+
+- Entry 1: [Code Challenge - Reverse an Array](https://bradley-hower.github.io/data-structures-and-algorithms/python/assignments/class-01)
+- Entry 2: [Code Challenge - Array Insert Shift](https://bradley-hower.github.io/data-structures-and-algorithms/python/assignments/class-02)
+- Entry 3: [Code Challenge - Binary Search](https://bradley-hower.github.io/data-structures-and-algorithms/python/assignments/class-03)
+- Entry 4: [Code Challenge - Matrix Sum](https://bradley-hower.github.io/data-structures-and-algorithms/python/assignments/class-04)
+- Entry 5: [Code Challenge - Linked List](https://bradley-hower.github.io/data-structures-and-algorithms/python/assignments/class-05)
+
+---
+Module 2
---
+++ python/assignments/class-05/linked_list_pack/linked_list.py
@@ -0,0 +1,45 @@
+class LinkedList:
+    """
+    Contains identifier of head, has string generation output of linkedlist, can insert new node and verify if a value is in linked list.
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(f'{{ {node.value} }}')
+            node = node.next
+        nodes.append("NULL")
+        return " -> ".join(nodes)
+
+    def some_method(self):
+        # method body here
+        pass
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def includes(self, value):
+        current_node = self.head
+        while(current_node):
+            if(value == current_node.value):
+                return True
+            current_node = current_node.next
+
+class Node:
+    "Holds value and indicates next node"
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class TargetError:
+    pass
